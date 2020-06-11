7b3ff42f5915ad533bc37d72f3481fc9d0ee8158
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-06-11 16:15:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,4 +7,9 @@
 def position_taken?(board, position)
   
   if board[position]==" " || board[position]== "" || board[position]==nil
-    
+    return false
+  elsif board[position]=="X" || board[position]=="O"
+    return true
+  end 
+
+end
